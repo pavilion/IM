2b96cbf4e3a9f8f570319e43fb256cbf409198d4
Ismael Oliva <ismael.oliva@digiseq.co.uk>
2019-12-18 17:05:15 +0000

    Updates

---
+++ precache-manifest.01f7dc48672ef7e54684e65e2ee3a80d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b934972516675a2fe39f89d6c478f096",
+    "url": "/IM/index.html"
+  },
+  {
+    "revision": "563806e1608f7e355ea6",
+    "url": "/IM/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "607c4a231956f57c9705",
+    "url": "/IM/static/js/2.1381923c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/IM/static/js/2.1381923c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "563806e1608f7e355ea6",
+    "url": "/IM/static/js/main.426c063c.chunk.js"
+  },
+  {
+    "revision": "74ecbe494a202b54ab47",
+    "url": "/IM/static/js/runtime-main.b2ecef3c.js"
+  }
+]);
